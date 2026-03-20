3ea883eeb2c17b48fbbc730e2c84f7cc1254de33
EC2 Default User <ec2-user@ip-172-31-42-113.ec2.internal>
2026-03-20 02:35:59 +0000

    new setup

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ec2-user/environment/cloudmail-api/venv
+set -gx VIRTUAL_ENV /home/ec2-user/environment/Email_API/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
